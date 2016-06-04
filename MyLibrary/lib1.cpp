6943a7eaf0ab13d78ade23804389aeccb87897ed
Pascal Thomet <pthomet@ivsweb.com>
2016-06-04 20:01:59 +0200

    More interestings tests

--- MyLibrary/lib1.cpp
+++ MyLibrary/lib1.cpp
@@ -5,8 +5,23 @@
 void HelloLibrary(){
     printf("Hello2 Library");
 }
-TEST_CASE("Basic Test") {
-  static int ashram = 2;
-  CHECK(ashram == 2);
-  ashram++;
+
+
+int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
+
+TEST_CASE("testing the factorial function") {
+    CHECK(factorial(1) == 1);
+    CHECK(factorial(2) == 2);
+    CHECK(factorial(10) == 3628800);
+}
+
+
+int fibo(int number) { return number < 2 ? number : fibo(number - 2) + fibo(number - 1); }
+
+TEST_CASE("testing the fibo function") {
+    CHECK(fibo(0) == 0);
+    CHECK(fibo(1) == 1);
+    CHECK(fibo(2) == 1);
+    CHECK(fibo(3) == 2);
+    CHECK(fibo(10) == 55);
 }
