37510ade17dbbd98fd4a509ac6ecf6a7b7e24248
Pascal Thomet <pthomet@ivsweb.com>
2016-06-05 11:57:18 +0200

    Alas ! Each file containing tests needs to be "registered" separately

--- MyLibrary/lib2.cpp
+++ MyLibrary/lib2.cpp
@@ -2,6 +2,10 @@
 #include "doctest.h"
 
 
+int DocTestRegister2() {
+    return 1;
+}
+
 TEST_CASE("testing in lib2.cpp") {
     std::cout << "testing in lib2.cpp" << std::endl;
     CHECK((1+1) == 2);
